2a91401c6e5baed3ade6d448dbbec35c49733f88
changhao-wang010220 <47792547+changhao-wang010220@users.noreply.github.com>
2022-06-09 21:38:58 -0400

    AM-38 create logout function on navbar
    AM-38 Create the log out function on the navbar and user can log out

--- src/frontend/src/components/Body/index.jsx
+++ src/frontend/src/components/Body/index.jsx
@@ -1,7 +1,9 @@
 import SignUp from '../../pages/SignUp'
 import {Route, Routes, Redirect} from 'react-router-dom'
 import Login from '../../pages/Login'
-export default function Body() {
+import React, {useState} from 'react'
+export default function Body(props) {
+
     return (
         <div className="p-5">
             
@@ -10,7 +12,7 @@ export default function Body() {
                 <Route path='/market' component={Market}/>
                 <Route path='/community' component={Community}/> */}
                 <Route path='signup' element={<SignUp/>}/>
-                <Route path='login' element={<Login/>}/>
+                <Route path='login' element={<Login updateLoginState={props.updateLoginState}/>}/>
                 {/* <Redirect to='/about'/> */}
             </Routes>
         </div>
