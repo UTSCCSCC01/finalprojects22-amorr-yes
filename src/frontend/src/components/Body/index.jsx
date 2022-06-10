438219baf190d097743be78197f95bf397bc2245
Theodliu <theodtheodliu@gmail.com>
2022-06-09 20:33:13 -0400

    update client-profile UI
    change 2 containers in client-profile into one card

--- src/frontend/src/components/Body/index.jsx
+++ src/frontend/src/components/Body/index.jsx
@@ -1,12 +1,30 @@
 import SignUp from '../../pages/SignUp'
+<<<<<<< Updated upstream
 import ClientProfile from '../../pages/ClientProfile'
 
+=======
+import {Route, Routes, Redirect} from 'react-router-dom'
+import Login from '../../pages/Login'
+import ClientProfile from '../../pages/ClientProfile'
+>>>>>>> Stashed changes
 export default function Body() {
     return (
         <div className="p-5">
             
+<<<<<<< Updated upstream
             {/* <SignUp/> */}
             <ClientProfile/>
+=======
+            <Routes>
+                {/* <Route path='/about' component={About}/>
+                <Route path='/market' component={Market}/>
+                <Route path='/community' component={Community}/> */}
+                <Route path='signup' element={<SignUp/>}/>
+                <Route path='login' element={<Login/>}/>
+                <Route path='clientprofile' element={<ClientProfile/>}/>
+                {/* <Redirect to='/about'/> */}
+            </Routes>
+>>>>>>> Stashed changes
         </div>
     )
 }
