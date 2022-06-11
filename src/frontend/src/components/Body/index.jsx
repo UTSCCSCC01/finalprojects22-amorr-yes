04da60bd6500720e6540f872c23759120ad7c3cc
changhao-wang010220 <47792547+changhao-wang010220@users.noreply.github.com>
2022-06-10 21:25:02 -0400

    change code and bug fix
    Clientprofile page bugs fix, add fake main page, update the navbar

--- src/frontend/src/components/Body/index.jsx
+++ src/frontend/src/components/Body/index.jsx
@@ -1,9 +1,10 @@
 import SignUp from '../../pages/SignUp'
-import {Route, Routes, Redirect} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import Login from '../../pages/Login'
 import ClientProfile from '../../pages/ClientProfile'
 import ClientIDUpload from '../../pages/ClientIDUpload'
-import React, {useState} from 'react'
+import React from 'react'
+import MainPage from '../../pages/MainPate'
 export default function Body(props) {
     return (
         <div className="p-5">
@@ -12,11 +13,11 @@ export default function Body(props) {
                 {/* <Route path='/about' component={About}/>
                 <Route path='/market' component={Market}/>
                 <Route path='/community' component={Community}/> */}
+                <Route path='' element={<MainPage/>}/>
                 <Route path='signup' element={<SignUp/>}/>
                 <Route path='clientprofile' element={<ClientProfile/>}/>
                 <Route path='login' element={<Login updateLoginState={props.updateLoginState}/>}/>
                 <Route path='clientidupload' element={<ClientIDUpload/>}/>
-                {/* <Redirect to='/about'/> */}
             </Routes>
         </div>
     )
--- src/frontend/src/pages/ClientProfile/index.jsx
+++ src/frontend/src/pages/ClientProfile/index.jsx
@@ -2,41 +2,40 @@ import React, {useState, useEffect} from 'react'
 import { useNavigate } from "react-router-dom"
 import axios from 'axios'
 export default function ClientProfile() {
-    const[first_name, setFirstName] = useState(null);
-    const[last_name, setLastName] = useState(null);
-    const[about, setAbout] = useState(null);
-    const[password, setPassword] = useState(null);
-    const[email, setEmail] = useState(null);
-    const[phone, setPhoneNumber] = useState(null);
-    const[gravatarphoto,setGravatarPhoto] = useState(null);
+    const[first_name, setFirstName] = useState("");
+    const[last_name, setLastName] = useState("");
+    const[about, setAbout] = useState("");
+    const[password, setPassword] = useState("");
+    const[email, setEmail] = useState("");
+    const[phone, setPhoneNumber] = useState("");
+    const[gravatarphoto,setGravatarPhoto] = useState("");
     const[same, setSame] = useState(true);
     const navigate = useNavigate();
-    var profile_change = 0;
+    let profile_change = 0;
     
-   
    
     function handleSave(){
         axios.post('/api/user_info_set/',{
-            
+            first_name: first_name,
+            last_name: last_name,
             about: about,
             email: email,
             phone: phone,
             password: password
         }).then(
             result => {
-                console.log('Success', result.data);
+                if(result.data.status === "succeeded") {
+                    alert("Saved successfully!");
+                    navigate("/clientprofile");
+                }
+                    
+                else
+                    alert("Saved unsuccessfully, please try again!")
             }, error => {
                 console.log('Error');
             }
         )
         profile_change = profile_change+ 1;
-        console.log(about)
-        console.log(email)
-        console.log(phone)
-        console.log(password)
-        console.log(first_name)
-        console.log(last_name)
-
     }
 
 
@@ -81,114 +80,95 @@ export default function ClientProfile() {
                        alert('Please Log in');
                        navigate('/login');
                    
-                   }else {
+                   } else {
                     setFirstName(result.data.first_name);
                     setLastName(result.data.last_name);
                     setAbout(result.data.about);
                     setEmail(result.data.email);
                     setPhoneNumber(result.data.phone);
-                    setGravatarPhoto(result.data.gravatarphoto);
-                    setGravatarPhoto("https://www.gravatar.com/avatar/"+{gravatarphoto});
+                    setGravatarPhoto("https://www.gravatar.com/avatar/" + result.data.gravatar_md5);
                 }
     
-                
-    
             }, rejection => {
-                console.log('Error', rejection.data);
+                console.log('Error');
             }
     
         )
     },[profile_change]);
     
-
     return (
         <div className="mdui-container p=3">
 
             <h2 className="mdui-text-center">
                 Client Profile
             </h2>
-
-            <div class="mdui-typo">
+            <div className="mdui-typo m=2">
                 <hr/>
             </div>
             <div className="mdui-container">
                 <div className="mdui-row">
                     <div className="mdui-card mdui-col-sm-6 mdui-col-xs-12 mdui-col-sm-8 mdui-col-lg-6 mdui-col-offset-sm-2 mdui-col-offset-lg-3">
-                        <div class="mdui-card-header">
-                            <img class="mdui-card-header-avatar" src={gravatarphoto}/>
-                            <input class="mdui-textfield-input mdui-col-md-4" type="text" defaultValue={first_name} onChange={getFirstName}/>
-                            <input class="mdui-textfield-input mdui-col-offset-md-4" type="text" defaultValue={last_name} onChange={getLastName}/>
+                        <div className="mdui-card-header">
+                            <img className="mdui-card-header-avatar" src={gravatarphoto} alt="gravatar"/>
+                            <a className="mdui-btn mdui-color-pink-accent mdui-ripple mdui-m-x-1" href="https://en.gravatar.com/">Gravatar</a>
                         </div>
-                {/* <div className="mdui-card-media">
-                <img className="mdui-img-fluid" src="https://s2.loli.net/2022/06/08/o2tTMzwHYS1RieW.png"/> */}
-                        <div className="mdui-card-media">
-                            <div className="mdui-card-primary">
+                        <div className="mdui-card-content">
+                            <input className="mdui-textfield-input mdui-col-md-4" type="text" defaultValue={first_name} onChange={getFirstName}/>
+                            <input className="mdui-textfield-input mdui-col-offset-md-4" type="text" defaultValue={last_name} onChange={getLastName}/>
+                            <div className="mdui-textfield">
+                                <label className="mdui-textfield-label">email</label>
+                                <input className="mdui-textfield-input" type="email" maxLength="32"  defaultValue={email} onChange={getEmail}/>
+                                <div className="mdui-textfield-error">Wrong Email Format</div>
                             </div>
-                {/* </div> */}
-                        </div>
-                        <div class="mdui-card-actions">
-                            <a className="mdui-btn mdui-color-pink-accent mdui-ripple"  href="https://en.gravatar.com/">Gravatar</a>
-                        </div>
-                        <div class="mdui-card-content">
-                        <div class="mdui-textfield">
-                            <label class="mdui-textfield-label">email</label>
-                            <input class="mdui-textfield-input" type="email" maxLength="32"  defaultValue={email} onChange={getEmail}/>
-                            <div className="mdui-textfield-error">Wrong Email Format</div>
+
+                            <div className="mdui-textfield">
+                                <label className="mdui-textfield-label">phone</label>
+                                <input className="mdui-textfield-input" maxLength="10" minLength="10"  defaultValue={phone} onChange={getPhoneNumber}/>
+                                <div className="mdui-textfield-error">Wrong Phone Format</div>
+                            </div>
+
+                            <div className="mdui-textfield">
+                                <label className="mdui-textfield-label">password</label>
+                                <input className="mdui-textfield-input" type="password" maxLength="32" pattern="^.*(?=.{6,}).*$" onChange={getPassword} required/>
+                                <div className="mdui-textfield-error">Password length has to be greater than 6 and less than 32!</div>
+                            </div>
+
+                            <div className={same?"mdui-textfield":"mdui-textfield mdui-textfield-invalid"}>
+                                <label className="mdui-textfield-label">Confirm Password</label>
+                                <input className="mdui-textfield-input" type="password" maxLength="32" onChange={confirm} required/>
+                                <div className="mdui-textfield-error">Password must be the same</div>
+                            </div>
+                        
+                        <div className="mdui-row">
+                            <h2 className="mdui-text-center">
+                                About Me
+                            </h2>
                         </div>
 
-                        <div class="mdui-textfield">
-                            <label class="mdui-textfield-label">phone</label>
-                            <input class="mdui-textfield-input" maxLength="10" minLength="10"  defaultValue={phone} onChange={getPhoneNumber}/>
-                            <div className="mdui-textfield-error">Wrong Phone Format</div>
+                    <div className="mdui-row">
+                        <div className="mdui-textfield mdui-m-l-2">
+                            <textarea className="mdui-textfield-input" rows="4" defaultValue={about} maxLength="200" onChange={getAbout} ></textarea>
                         </div>
-                
-                       
+                    </div>
                     
-                        <div className="mdui-textfield">
-                            <label class="mdui-textfield-label">password</label>
-                            <input class="mdui-textfield-input" type="password" maxLength="32" pattern="^.*(?=.{6,}).*$" onChange={getPassword} required/>
-                            <div className="mdui-textfield-error">Password length has to be greater than 6 and less than 32!</div>
+                    <button className="mdui-btn mdui-color-pink-accent mdui-ripple mdui-xm-4" mdui-dialog="{target: '#comfirmEdit'}">save</button>
+                    <div className="mdui-card-actions">
+                        <div className="mdui-dialog" id="comfirmEdit">
+                            <div className="mdui-dialog-title">Are you sure?</div>
+                            <div className="mdui-dialog-content">You'll edit your profile!</div>
+                            <div className="mdui-dialog-actions">
+                                <button className="mdui-btn mdui-ripple" mdui-dialog-close>cancel</button>
+                                <button className="mdui-btn mdui-ripple" onClick={handleSave}>comfirm</button>
+                            </div>
                         </div>
-                        
 
-                        
-                        <div className={same?"mdui-textfield":"mdui-textfield mdui-textfield-invalid"}>
-                            <label class="mdui-textfield-label">Confirm Password</label>
-                            <input class="mdui-textfield-input" type="password" maxLength="32" onChange={confirm} required/>
-                            <div className="mdui-textfield-error">Password must be the same</div>
-                        </div>
-                        
-            
-            
-                    <div className="mdui-row">
-                        <h2 className="mdui-text-center">
-                            About Me
-                        </h2>
-                    </div>
-
-                    <div className="mdui-row">
-                        <div class="mdui-textfield mdui-m-l-2">
-                            <textarea class="mdui-textfield-input" rows="4" defaultValue={about} maxLength="200" onChange={getAbout} ></textarea>
-                        </div>
-                    </div>
-
-                    <div class="mdui-card-actions">
-                        <button className="mdui-btn mdui-color-pink-accent mdui-ripple mdui-xm-4"  mdui-dialog="{target: '#exampleDialog'}">save</button>
-                        <div class="mdui-dialog" id="exampleDialog">
-                        <div class="mdui-dialog-title">Are you sure?</div>
-                        <div class="mdui-dialog-content">You'll edit your profile!</div>
-                        <div class="mdui-dialog-actions">
-                            <a class="mdui-btn mdui-ripple" mdui-dialog-confirm onClick={handleSave}>Edit</a>
-                        </div>
-                    </div>
-
-                    <div className="mdui-row">
-                        <div className="mdui-col mdui-col-xs-12 mdui-col-sm-8 mdui-m-t-1">
-                            <a  className="mdui-btn mdui-text-color-blue" onClick={handleIDUpload}><u>Upload Your Photo ID</u></a>
+                        <div className="mdui-row">
+                            <div className="mdui-col mdui-col-xs-12 mdui-col-sm-8 mdui-m-t-1">
+                                <a className="mdui-btn mdui-text-color-blue" onClick={handleIDUpload}><u>Upload Your Photo ID</u></a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             
             </div>
             
@@ -197,11 +177,7 @@ export default function ClientProfile() {
             </div> 
             
             </div>
-            <div className="mdui-container">
             
-            
-            
-            </div>
             
             
             
