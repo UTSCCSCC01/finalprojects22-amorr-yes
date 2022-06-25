37fc2b52e5dee1e37884e9a7ff03ba62f48a467c
changhao-wang010220 <47792547+changhao-wang010220@users.noreply.github.com>
2022-06-25 11:17:56 -0400

    create navbar for develop

---
+++ src/frontend/src/pages/ProviderLogin/index.jsx
@@ -0,0 +1,5 @@
+export default function ProviderLogin() {
+    return (
+        <h1 className="mdui-text-center">ProviderLogin</h1>
+    )
+}
---
+++ src/frontend/src/pages/ProviderProfile/index.jsx
@@ -0,0 +1,5 @@
+export default function ProviderProfile() {
+    return (
+        <h1 className="mdui-text-center">ProviderProfile</h1>
+    )
+}
