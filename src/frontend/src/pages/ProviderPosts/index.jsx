735e735dd67cb28edbb6c6168cfc8aa1b31cb896
changhao-wang010220 <47792547+changhao-wang010220@users.noreply.github.com>
2022-07-01 23:56:11 -0400

    change posts ui design

--- src/frontend/src/pages/ProviderPosts/index.jsx
+++ src/frontend/src/pages/ProviderPosts/index.jsx
@@ -36,31 +36,36 @@ export default function ProviderPosts(props) {
 
     return (
         <div className="mdui-container">
-            <div className="mdui-table-fluid">
-                <table className="mdui-table mdui-table-hoverable">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Title</th>
-                            <th>Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            postsList.map(post => {
-                                return (
-                                    <tr onClick={() => handleClick(post.pid)}>
-                                        <td>{post.pid}</td>
-                                        <td>{post.title}</td>
-                                        <td>{post.start_time + ` - ` + post.end_time}</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
+            <div className="mdui-col-xs-12 mdui-col-sm-8 mdui-col-lg-6 mdui-col-offset-sm-2 mdui-col-offset-lg-3">
+                <div className="mdui-table-fluid">
+                    <table className="mdui-table mdui-table-hoverable">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Title</th>
+                                <th>Time</th>
+                                <th>Provider</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                postsList.map(post => {
+                                    return (
+                                        <tr onClick={() => handleClick(post.pid)}>
+                                            <td>{post.pid}</td>
+                                            <td>{post.title}</td>
+                                            <td>{post.start_time + ` - ` + post.end_time}</td>
+                                            <td>{post.author_first_name + post.author_last_name}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+                <button className="mdui-btn mdui-color-pink-accent mdui-ripple mdui-m-t-2" onClick={handleCreate}>Create new post</button>
             </div>
-            <button className="mdui-btn mdui-color-pink-accent mdui-ripple mdui-m-t-2" onClick={handleCreate}>Create new post</button>
+            
         </div>
         
     )
