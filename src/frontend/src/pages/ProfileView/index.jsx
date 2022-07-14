3ac635e96f7199e0abc55ba9be376ecb4551e86c
Theodliu <theodtheodliu@gmail.com>
2022-07-14 19:46:13 -0400

    bug fix profile view
    bug fix profile view: new page when click post

--- src/frontend/src/pages/ProfileView/index.jsx
+++ src/frontend/src/pages/ProfileView/index.jsx
@@ -25,8 +25,10 @@ export default function ProfileView() {
 
 
     function handleClick(pid) {
-        navigate('/postview/' + pid);
-    }  
+        const w = window.open('_blank');
+        let url = "../../postview/" + pid;
+        w.location.href = url;
+    }
 
     useEffect(() => {
         
