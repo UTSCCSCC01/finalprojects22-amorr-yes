b7c88bcdd5e1bfbe21e0a9b4e49150862bdba339
Theodliu <94936564+Theodliu@users.noreply.github.com>
2022-07-29 16:21:37 -0400

    Upload AM-75 admin account frontend (#60)
    Upload AM-75 admin account frontend

---
+++ src/frontend/src/pages/AdminLogin/index.jsx
@@ -0,0 +1,66 @@
+import React, {useState} from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
+export default function AdminLogin(props) {
+
+    const[accesscode, setAccessCode] = useState(null);
+    const navigate = useNavigate();
+
+
+    function handleLogin() {
+        axios.post('/api/adminlogin/', {
+            accesscode: accesscode
+        }).then(
+            resolution => {
+                console.log('Success', resolution.data);
+                if (resolution.data['status'] === 'failed') {
+                    alert('Incorrect access code.');
+                    window.location.reload();
+                }else if (resolution.data['status'] === 'succeeded') {
+                    console.log('Redirecting...');
+                    props.updateAdminLoginState();
+                    axios.get("/api/admin/").then(
+                        resolution => {
+                            console.log('Success', resolution.data);
+                            navigate('/admin');
+                        }, rejection => {
+                            console.log('Error', rejection.data);
+                            alert("Sth error");
+                            navigate('/adminlogin');
+                        }
+                    )
+                    
+                }
+            }, rejection => {
+                console.log('Error');
+                alert("Sth error");
+            }
+        )
+    }
+
+
+    function getAccessCode(event) {
+        setAccessCode(event.target.value);
+    }
+
+    return (
+        <div className="mdui-container p=3">
+
+            <h2 className="mdui-text-center">
+                Administrator Login
+            </h2>
+            <div className="mdui-row">
+                <div className="mdui-textfield mdui-textfield-floating-label mdui-col-xs-12 mdui-col-sm-8 mdui-col-lg-6 mdui-col-offset-sm-2 mdui-col-offset-lg-3">
+                    <label className="mdui-textfield-label">Access Code</label>
+                    <input className="mdui-textfield-input" type="password" maxLength="32" onChange={getAccessCode} required/>
+                </div>
+            </div>
+
+            <div className="mdui-row mdui-m-t-5">
+                <div className="mdui-col mdui-col-xs-12 mdui-col-sm-8 mdui-col-lg-6 mdui-col-offset-sm-2 mdui-col-offset-lg-3">
+                    <button className="mdui-btn mdui-btn-block mdui-color-pink-accent mdui-ripple" onClick={handleLogin}>Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/frontend/src/pages/AdminPhotoId/index.jsx
@@ -0,0 +1,67 @@
+import mdui from 'mdui'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { useNavigate } from "react-router-dom"
+
+export default function AdminPhotoId() {
+    const navigate = useNavigate();
+
+    const[users, setUsers] = useState([]);
+    let change = 0;
+    useEffect(() => {
+        mdui.mutation();
+        axios.get("/api/get_user_list/").then(
+            result => {
+                if (result.data.status === 'succeeded') {
+                    setUsers(result.data.result);
+                }
+                else {
+                    alert('load list failed, please try again.');
+                }
+            }, error => {
+                console.log("error")
+            }
+        )
+    }, [change])
+
+    function handleClick(uid) {
+        const w = window.open('_blank');
+        let url = "../providerview/" + uid;
+        w.location.href = url;
+    }
+
+
+    return(
+        <div className="mdui-container">
+            <h2 className="mdui-text-center">
+                All user
+            </h2>
+            <div className="mdui-col-xs-12 mdui-col-sm-10 mdui-col-lg-8 mdui-col-offset-sm-1 mdui-col-offset-lg-2">
+                <div className="mdui-table-fluid">
+                    <table className="mdui-table mdui-table-hoverable">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map(user => {
+                                    return (
+                                        <tr onClick={() => handleClick(user.uid)}>
+                                            <td>{user.first_name + user.last_name}</td>
+                                            {/* <td>{user.lastname + user.firstname}</td> */}
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
